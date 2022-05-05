fbe6989d8666de9e3a24beac9ca89b17064382b5
xiabee <xiabee@foxmail.com>
2022-05-05 13:17:56 +0800

    update

--- target/www/index.php
+++ target/www/index.php
@@ -50,34 +50,32 @@
                                 <div class="control">
                                     <button class="button is-primary" type="submit">Query</button>
                                     <?php
-                                    include "./config.php";
-                                    // error_reporting(0);
+                                        include "./config.php";
+                                        // error_reporting(-1);
+                                        // highlight_file(__FILE__);
 
-                                    // highlight_file(__FILE__);
+                                        $conn = mysqli_connect($hostname, $username, $password, $database);
+                                        if ($conn->connect_errno) {
+                                            die("Connection failed: " . $conn->connect_errno);
+                                            # php版本升级，报错处理暂时没有处理好
+                                        } 
 
-                                    $conn = mysqli_connect($hostname, $username, $password, $database);
-
-                                    if (mysqli_connect_errno($conn)) {
-                                        die("CONNECTION ERROR" . mysqli_connect_error());
-                                    }
-
-                                    if (isset($_GET['id'])) {
-                                        $id = $_GET['id'];
-                                        if ($id == null)
-                                            die;
-                                        $sql = "select * from users where id=$id";
-                                        $result = mysqli_query($conn, $sql);
-                                        if ($result) {
-                                            $row = mysqli_fetch_array($result);
-                                            echo "<h3>WELCOME " . $row['username'] . "<h3><br>";
-                                            echo "<h3>USERNAME   :   " . $row['username'] . "</h3><br>";
-                                            echo "<h3>PASSWORD   :   " . $row['password'] . "</h3>";
-                                        } else {
-                                            die('<br>WRONG! ');
+                                        if (isset($_GET['id'])) {
+                                            $id = $_GET['id'];
+                                            $sql = "select * from users where id=$id";
+                                            $result = mysqli_query($conn, $sql);
+                                            if ($result) {
+                                                $row = mysqli_fetch_array($result);
+                                                echo "<h3>" . $row['username'] . "</h3><br>";
+                                                echo "<h3>" . $row['passwd'] . "</h3>";
+                                            }
+                                            else {
+                                                die('<br>.Could not connect: '.mysqli_errno($conn));
+                                                # php版本升级，报错处理暂时没有处理好
+                                            }
                                         }
-                                    }
-                                    mysqli_close($con);
-                                    ?>
+                                        mysqli_close($con); 
+                                        ?>
                                 </div>
                             </form>
                         </li>
